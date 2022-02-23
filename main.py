ade45df427320ed3d5a2e4d355986c9e0e17a4a9
andytam101 <92043942+andytam101@users.noreply.github.com>
2022-02-23 17:38:47 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,43 @@
 from image_slicing import *
-# from ndvi import *
-# from ImageCaptureTest import *
+from ndvi import *
+from ImageCaptureTest import *
 from pathlib import *
 import time
 
+if __name__ == "__main__":
 
-# setting up photo directory
+    photoDestination = Path("./CapturedImages")
+    if not photoDestination.exists():
+        photoDestination.mkdir()
 
-photoDestination = Path("./CapturedImages")
-if not photoDestination.exists():
-    photoDestination.mkdir()
+    counter = 0
+    previous = time.time() - 5 
 
-counter = 0
-stopwatch = 5
+    while counter < 10:
+        current = time.time()
+        if current - previous <= 3: continue
+        else:
+            previous = current
+        photoPath = Path(f"./CapturedImages/Image{counter}")
+        if not photoPath.exists():
+            photoPath.mkdir()
+        elif photoPath.stat().st_size == 0:
+            counter += 1
 
-previous = time.time()
+        originalImage = Path(take_pic(photoPath))
 
-# iteration
+        # oImage = Image.open(f"./Sample_Images/cslab3ogel_Files_RawData_raw_image_{counter + 1}.jpeg")
+        # originalImage = f"{photoPath}/unslicedPhoto.jpeg"
+        # oImage.save(originalImage)
 
-while counter < 10:
-    current = time.time()
-    if current - previous <= 4: continue
-    else:
-        previous = current
-    photoPath = Path(f"./CapturedImages/Image{counter}")
-    if not photoPath.exists():
-        photoPath.mkdir()
-    elif photoPath.stat().st_size == 0:
+        slice(photoPath,100,100,30)
+
+        Path(originalImage).unlink()
         counter += 1
 
-    # originalImage = Path(take_pic(photoPath))
-
-    oImage = Image.open(f"./Sample_Images/cslab3ogel_Files_RawData_raw_image_{counter + 1}.jpeg")
-    originalImage = f"{photoPath}/unslicedPhoto.jpeg"
-    oImage.save(originalImage)
-
-    slice(photoPath,100,100,30)
-
-    Path(originalImage).unlink()
-
-    counter += 1
-    print(f"Photo {counter} done")
+    # testIm = Image.open("./Sample_Images/cslab3ogel_Files_RawData_raw_image_1.jpeg")
+    # testIm.save("./test_slice/original.jpeg")
+    # new = contrast_stretch(testIm)
+    # ndvi = calc_ndvi(new)
+    # print(ndvi)
+    # input()
